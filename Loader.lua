7d094830e11b683831b3ef5a2225a79c439d080f
futurewayys <henokhdamanik361@gmail.com>
2025-12-05 04:34:08 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,5 +2,5 @@
 
 local GameId = game.GameId
 if GameId == 7074860883 then
-  loadstring(game:HttpGet("https://github.com/futurewayys/Executive/raw/dev/Game/FishIt.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/futurewayys/executive/main/Game/FishIt.lua"))()
 end
